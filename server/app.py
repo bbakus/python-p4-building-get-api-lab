f4c24b03cea1f7c93dc754038f860cf838b6b0d7
Brandon Bakus <brandonbakus@gmail.com>
2025-03-12 20:52:36 -0400

    okay this wasnt too bad

--- server/app.py
+++ server/app.py
@@ -20,19 +20,36 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    all_bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
+    return all_bakeries
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    
+    bakery = Bakery.query.get(id)
+
+    bakery_dict = bakery.to_dict()
+    bakery_dict["baked_goods"] = [baked_good.to_dict() for baked_good in bakery.baked_goods]
+    
+    return bakery_dict
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods_by_price = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    
+    baked_price = [baked_good.to_dict() for baked_good in baked_goods_by_price]
+    
+    return baked_price
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    highest_price = BakedGood.query.order_by(BakedGood.price.desc()).first()
+
+    return jsonify(highest_price.to_dict())
+
+
+
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
